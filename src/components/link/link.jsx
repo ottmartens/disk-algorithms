c45dabd13f69310880e8d6280ccdc28a3120cec5
Ott Martens <ott.martens@pipedrive.com>
2019-12-16 15:20:29 +0200

    Chart and styles

--- src/components/link/link.jsx
+++ src/components/link/link.jsx
@@ -3,20 +3,10 @@ import React from 'react';
 import styles from './link.module.scss';
 
 const Link = () => (
-  <>
-    <a
-      className={styles.link}
-      href='https://ottmartens.github.io/cpu-algorithms/'
-    >
-      CPU Scheduler
-    </a>
-    <a
-      className={styles.link}
-      href='https://ottmartens.github.io/ram-algorithms/'
-    >
-      RAM Scheduler
-    </a>
-  </>
+  <div className={styles.link}>
+    <a href="https://ottmartens.github.io/cpu-algorithms/">CPU Scheduler</a>
+    <a href="https://ottmartens.github.io/ram-algorithms/">RAM Scheduler</a>
+  </div>
 );
 
 export default Link;
--- src/components/visualizer/visualizer.jsx
+++ src/components/visualizer/visualizer.jsx
@@ -1,4 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Label,
+  ResponsiveContainer
+} from 'recharts';
 
 import { useInterval } from '../../hooks';
 
@@ -15,12 +24,13 @@ const Visualizer = ({
   error
 }) => {
   const [stepCount, setStepCount] = useState(0);
+  const [scannedPositions, setScannedPositions] = useState([10]);
 
   useInterval(
     () => {
       step();
     },
-    started ? 1000 : null
+    started ? 500 : null
   );
 
   useEffect(() => {
@@ -31,14 +41,61 @@ const Visualizer = ({
   }, [setError, started]);
 
   const step = () => {
-    console.log(stepCount);
+    if (stepCount > 6) setStarted(false);
+    setScannedPositions(
+      scannedPositions.concat([Math.floor(Math.random() * 50)])
+    );
     setStepCount(stepCount + 1);
   };
 
   return (
     <div className={styles.visualizer}>
-      {pattern}
-      {algorithm}
+      <ResponsiveContainer
+        width="90%"
+        height={(scannedPositions.length + 1) * 40}
+      >
+        <LineChart
+          layout="vertical"
+          data={scannedPositions.map((value, index) => ({
+            index,
+            value
+          }))}
+        >
+          <XAxis
+            domain={[0, 49]}
+            tickCount={50}
+            interval={0}
+            orientation="top"
+            type="number"
+          >
+            <Label
+              offset={-5}
+              position="insideTop"
+              value="Head position"
+              style={{
+                fontSize: '85%',
+                fill: 'rgba(255, 255, 255, 0.87)'
+              }}
+            />
+          </XAxis>
+          <YAxis
+            domain={[0, scannedPositions.length]}
+            ticks={[...scannedPositions.keys()]}
+            interval={0}
+            dataKey="index"
+            type="number"
+          />
+          <Line dataKey="value" stroke="#8884d8" />
+          <Tooltip
+            labelFormatter={label =>
+              label === 0 ? `Initial position` : `Step: ${label}`
+            }
+            formatter={value => [`Position: ${value}`, null]}
+            isAnimationActive={false}
+            active={!started && scannedPositions.length > 1}
+          />
+        </LineChart>
+      </ResponsiveContainer>
       <Counter stepCount={stepCount} />
     </div>
   );
