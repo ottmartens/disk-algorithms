6533a3d5c09c6eba4cf703d9e65ee76d9e5d02af
Ott Martens <ott.martens@pipedrive.com>
2019-12-23 15:00:17 +0200

    Updates

---
+++ precache-manifest.af8150c083beb510575d6d08d57bb250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194d4e237cf86440a2d240c30c3d2e0a",
+    "url": "/disk-algorithms/index.html"
+  },
+  {
+    "revision": "e312b01ad05a8a8c8eda",
+    "url": "/disk-algorithms/static/css/main.e29228ca.chunk.css"
+  },
+  {
+    "revision": "1f41a07cb6d240b02deb",
+    "url": "/disk-algorithms/static/js/2.f53a4680.chunk.js"
+  },
+  {
+    "revision": "efe80d506e138c343a03a1541511a2f3",
+    "url": "/disk-algorithms/static/js/2.f53a4680.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e312b01ad05a8a8c8eda",
+    "url": "/disk-algorithms/static/js/main.6066cc40.chunk.js"
+  },
+  {
+    "revision": "457ed099f914b22f8b9c",
+    "url": "/disk-algorithms/static/js/runtime-main.cdde81c1.js"
+  }
+]);
