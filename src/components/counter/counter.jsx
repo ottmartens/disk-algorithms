06c749ce9b9e085c36ddcfee14c6e5017b7edd3a
Ott Martens <ott.martens@pipedrive.com>
2019-12-23 14:55:13 +0200

    Total travel

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import styles from './counter.module.scss';
 
-const Counter = ({ stepCount }) => (
-  <div className={styles.counter}>
-    <div>
-      tick count: <span className={styles.number}>{stepCount}</span>
+const Counter = ({ stepCount, scannedPositions }) => {
+  const [distance, setDistance] = useState(0);
+
+  useEffect(() => {
+    let dist = 0;
+    for (let i = 1; i < scannedPositions.length; i++) {
+      const position = scannedPositions[i];
+      const lastPosition = scannedPositions[i - 1];
+      if (position > lastPosition) dist += position - lastPosition;
+      else dist += lastPosition - position;
+    }
+
+    setDistance(dist);
+  }, [scannedPositions]);
+
+  return (
+    <div className={styles.counter}>
+      <div>
+        total travel: <span className={styles.number}>{distance}</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Counter;
